44b8a25c235332ecfdc9b9f621c9e33875820c8f
jnyhs <jennyhaner@gmail.com>
2022-07-09 14:57:22 -0300

    firs commit

---
+++ src/com/PrimeiroProjeto/PrimeroProjeto.java
@@ -0,0 +1,14 @@
+package com.PrimeiroProjeto;
+
+public class PrimeroProjeto {
+
+	public static void main(String[] args) {
+		
+		int a = 2;
+		int b = 3;
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!" + (a+b));
+
+	}
+
+}
---
+++ src/com/PrimeiroProjeto/model/Gato.java
@@ -0,0 +1,5 @@
+package com.PrimeiroProjeto.model;
+
+public class Gato {
+
+}
